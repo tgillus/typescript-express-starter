257f525c3bf154906c99a107b27ba3692baa6fbc
Tramaine Gillus <tramaine.gillus@gmail.com>
2021-07-17 19:37:43 -0400

    Add logging.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import 'source-map-support/register';
 import './util/env';
 import { app } from './server/app';
+import { logger } from './util/logger';
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}.`);
+  logger.info(`Listening on port ${PORT}.`);
 });
