d7e6669f24eb2b42a6a88a045d42be95545e47d1
Tramaine Gillus <tramaine.gillus@gmail.com>
2021-08-15 00:17:22 -0400

    Add Knex.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,29 @@
 import './util/env';
 import { app } from './express/app';
 import { logger } from './util/logger';
+import knex from 'knex';
+
+knex({
+  client: 'sqlite3',
+  connection: {
+    filename: './database.sqlite',
+  },
+  useNullAsDefault: true,
+});
+// knex({
+//   client: 'sqlite3',
+//   connection: {
+//     filename: 'file:memDb1?mode=memory',
+//   },
+//   useNullAsDefault: true,
+// });
+// interface User {
+//   id: number;
+//   name: string;
+//   age: number;
+// }
+
+// knex<User>('users').where('id').first();
 
 const PORT = process.env.PORT;
 
