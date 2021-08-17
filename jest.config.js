46709ffbf9657f1bdf99f451078dd302a655dfd3
Tramaine Gillus <tramaine.gillus@gmail.com>
2021-08-16 21:33:40 -0400

    Add test coverage threshold checks.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,15 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/.tmp-build/', '<rootDir>/.tmp-build/'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 75,
+      lines: 80,
+      statements: 80,
+    },
+    './src/util/': {
+      branches: 0,
+    },
+  },
 };
